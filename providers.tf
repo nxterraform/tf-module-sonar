adfb44d6dd0a8ff061fab7f65bbeb38725f5b06a
dhirwanashish <ashish.dhirwan@successive.tech>
2020-09-10 13:44:02 +0530

    fix

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
-provider "helm" {
+# provider "helm" {
   
-  kubernetes {
-    host = "https://240D2B04916CFB6E91311591E0BEBE0F.sk1.us-west-2.eks.amazonaws.com"
-    # config_path = "~/.kube/config"
+#   kubernetes {
+#     host = "https://240D2B04916CFB6E91311591E0BEBE0F.sk1.us-west-2.eks.amazonaws.com"
+#     # config_path = "~/.kube/config"
   
-    }
-  }
+#     }
+#   }
