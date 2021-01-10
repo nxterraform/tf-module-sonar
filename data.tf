2bd9ea700818de027cecffa1567ac2f693dd0995
dhirwanashish <ashish.dhirwan@successive.tech>
2021-01-10 18:43:46 +0530

    data helm release depricated

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "helm_repository" "sonar" {
-  name = "sonarqube"
-  url  = "https://oteemo.github.io/charts"
-}
+# data "helm_repository" "sonar" {
+#   name = "sonarqube"
+#   url  = "https://oteemo.github.io/charts"
+# }
--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ resource "helm_release" "sonar" {
   name  = "sonarqube-nx"
   chart = "sonarqube"
   namespace = var.namespace
-  repository = data.helm_repository.sonar.url
+  # repository = data.helm_repository.sonar.url
+  repository = "https://oteemo.github.io/charts"
   create_namespace = true
   atomic = true
   
